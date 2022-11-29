6bdac6b84b549bb7d7f2494533e1e4ed1972827b
HiroIshida <spitfire.docomo@gmail.com>
2022-11-29 16:25:40 +0900

    Fix symbolic link

--- action.sh
+++ action.sh
@@ -1,7 +1,7 @@
 logpath=$HOME/.mohou/plug_insert
 rosbagpath=$logpath/rosbag
 
-ln -sf config/* $logpath
+ln -sf $(pwd)/config/* $logpath
 ln -sf $rosbagpath ./
 latest_rosbag_name=$(ls $rosbagpath -tl|grep train-episode|head -1|awk '{print $9}')
 latest_rosbag_path=$rosbagpath/$latest_rosbag_name
